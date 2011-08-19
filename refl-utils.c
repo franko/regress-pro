6485abb20d15d6a7f0787335a50cc7ef44ce32b9
Francesco Abbate <francesco.bbt@gmail.com>
2011-08-19 23:44:59 +0200

    Implemented method to load natively NOVA spectra

--- refl-utils.c
+++ refl-utils.c
@@ -1,7 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "refl-utils.h"
 #include "error-messages.h"
 #include "data-table.h"
 
+#define NORMALIZE(c) ((float) c / 32020.0)
+
+static double
+get_lambda (double w[], int k)
+{
+  return w[0] + k * w[1] + k*k * w[2];
+}
+
+struct data_table *
+read_nova_spectrum (FILE *f, str_ptr ln)
+{
+  struct data_table *table;
+  unsigned int c, c_save;
+  int na, j, lc0 = 0, lcount;
+  long dpos;
+  int starting_zeroes = 1, ending_zeroes = 0;
+  double w[5];
+
+  for (;;)
+    {
+      char tc;
+      if (str_getline (ln, f) < 0)
+	break;
+      na = sscanf (CSTR(ln), "%u %c", &c, &tc);
+
+      if (na == 1)
+	{
+	  if (c > 0 && starting_zeroes)
+	    {
+	      c_save = c;
+	      dpos = ftell (f);
+	      starting_zeroes = 0;
+	      lcount = 1;
+	      continue;
+	    }
+
+	  if (c == 0)
+	    {
+	      if (!starting_zeroes)
+		{
+		  ending_zeroes = 1;
+		}
+	      else
+		{
+		  lc0++;
+		}
+	    }
+
+	  if (!starting_zeroes && !ending_zeroes)
+	    lcount++;
+	  continue;
+	}
+      else if (na == 2 && lcount > 0)
+	{
+	  const char *p = CSTR(ln);
+	  if (sscanf (p, "%lf %lf %lf %lf %lf \n", w, w+1, w+2, w+3, w+4) == 5)
+	    break;
+	}
+
+      return NULL;
+    }
+
+  if (fseek (f, dpos, SEEK_SET) < 0)
+    return NULL;
+
+  table = data_table_new (lcount, 2);
+  data_table_set (table, 0, 0, get_lambda(w, lc0+1));
+  data_table_set (table, 0, 1, NORMALIZE(c_save));
+
+  for (j = 1; j < lcount; j++)
+    {
+      fscanf (f, " %u", &c);
+
+      data_table_set (table, j, 0, get_lambda(w, lc0+j+1));
+      data_table_set (table, j, 1, NORMALIZE(c));
+    }
+
+  return table;
+}
 
 struct spectrum *
 load_refl_data (const char *filename)
@@ -22,14 +104,21 @@ load_refl_data (const char *filename)
 
   str_init (ln, 64);
 
-  str_getline (ln, f);
-  str_getline (ln, f);
-
   s = emalloc(sizeof(struct spectrum));
-
   s->config.system = SYSTEM_REFLECTOMETER;
 
-  table = data_table_read_lines (f, "%*s %f %*f %f %*f\n", 0, 2);
+  str_getline (ln, f);
+
+  if (strstr (CSTR(ln), ";Experimental Spectrum") || \
+      strstr (CSTR(ln), ";Theoretical Spectrum"))
+    {
+      table = read_nova_spectrum (f, ln);
+    }
+  else
+    {
+      str_getline (ln, f);
+      table = data_table_read_lines (f, "%*s %f %*f %f %*f\n", 0, 2);
+    }
 
   if (table == NULL)
     goto invalid_s;
