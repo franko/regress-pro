franko/regress-pro

commit d494320a9f47f3db24422c06cd4735061c746578

Add functions to add or insert a layer in film stack

stack.c
@@ -18,22 +18,26 @@ stack_init(stack_t *s)
     stack_init_raw(s, 8);
 }
 
+static void
+ensure_size(stack_t *s, int req_size)
+{
+    if((size_t)req_size > s->nb_alloc)
+    {
+        size_t nsz;
+        s->nb_alloc *= 2;
+        nsz = s->nb_alloc;
+        s->disp = erealloc(s->disp, nsz * sizeof(void *));
+        /* For thicknesses we allocate more space of what is actually needed. */
+        s->thickness = erealloc(s->thickness,  nsz * sizeof(double));
+    }
+}
+
 void
 stack_add_layer(stack_t *s, disp_t *lyr, double th)
 {
     size_t idx;
 
-    if((size_t)s->nb >= s->nb_alloc) {
-        size_t nsz;
-
-        s->nb_alloc *= 2;
-        nsz = s->nb_alloc;
-
-        s->disp = erealloc(s->disp, nsz * sizeof(void *));
-
-        /* For thicknesses we allocate more space of what is actually needed. */
-        s->thickness = erealloc(s->thickness,  nsz * sizeof(double));
-    }
+    ensure_size(s, s->nb + 1);
 
     idx = s->nb;
 
@@ -43,6 +47,33 @@ stack_add_layer(stack_t *s, disp_t *lyr, double th)
     }
 
     s->nb ++;
+}
+
+void
+stack_insert_layer(stack_t *s, int pos, disp_t *lyr, double th)
+{
+    ensure_size(s, s->nb + 1);
+    int nrem = s->nb - pos;
+    memcpy(s->disp + pos + 1, s->disp + pos, sizeof(void *) * nrem);
+    int lpos = pos > 0 ? pos - 1 : 0;
+    memcpy(s->thickness + lpos + 1, s->thickness + lpos, sizeof(double) * nrem);
+    s->disp[pos] = lyr;
+    if (pos > 0) {
+        s->thickness[pos - 1] = th;
+    } else {
+        s->thickness[0] = 0.0;
+    }
+    s->nb++;
+}
+
+void
+stack_delete_layer(stack_t *s, int pos)
+{
+    int nrem = s->nb - pos - 1;
+    memcpy(s->disp + pos, s->disp + pos + 1, sizeof(void *) * nrem);
+    int lpos = pos > 0 ? pos - 1 : 0;
+    memcpy(s->thickness + lpos, s->thickness + lpos + 1, sizeof(double) * nrem);
+    s->nb--;
 }
 
 stack_t *

stack.h
@@ -22,6 +22,8 @@ extern stack_t *stack_copy(stack_t *s);
 extern void     stack_init(stack_t *s);
 extern void     stack_init_raw(stack_t *s, size_t nb_init);
 extern void     stack_add_layer(stack_t *s, disp_t *lyr, double th);
+extern void     stack_insert_layer(stack_t *s, int pos, disp_t *lyr, double th);
+extern void     stack_delete_layer(stack_t *s, int pos);
 extern const
 double *        stack_get_ths_list(const stack_t *s);
 extern void     stack_get_ns_list(stack_t *s, cmpl *ns, double lambda);

commit becf88e7f1e3dce30102738bdd564a0655b7cf22

Fix memory leak introduced with d494320a
In function stack_delete_layer dispose the dispersion object of the
deleted layer.

stack.c
@@ -70,6 +70,7 @@ void
 stack_delete_layer(stack_t *s, int pos)
 {
     int nrem = s->nb - pos - 1;
+    disp_free(s->disp[pos]);
     memcpy(s->disp + pos, s->disp + pos + 1, sizeof(void *) * nrem);
     int lpos = pos > 0 ? pos - 1 : 0;
     memcpy(s->thickness + lpos, s->thickness + lpos + 1, sizeof(double) * nrem);