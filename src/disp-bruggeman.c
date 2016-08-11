3c27a084b84f6fd53dd7a5f4f49e2532ac970009
Francesco Abbate <francesco.bbt@gmail.com>
2016-08-11 16:56:15 +0200

    First implementation of Bruggeman dispersion model
    The UI counterpart is not yet implemented.
    
    Some debug code is present in the implementation and should be
    removed once tests are terminated.

--- src/disp-bruggeman.c
+++ src/disp-bruggeman.c
@@ -17,6 +17,9 @@ static void bruggeman_encode_param(str_t param, const fit_param_t *fp);
 
 static double bruggeman_get_param_value(const struct disp_struct *d,
                                         const fit_param_t *fp);
+static double * bruggeman_map_param(disp_t *d, int index);
+static int bruggeman_write(writer_t *w, const disp_t *_d);
+static int bruggeman_read(lexer_t *l, disp_t *d);
 
 struct disp_class bruggeman_disp_class = {
     .disp_class_id       = DISP_BRUGGEMAN,
@@ -30,29 +33,56 @@ struct disp_class bruggeman_disp_class = {
     .fp_number           = bruggeman_fp_number,
     .n_value_deriv       = bruggeman_n_value_deriv,
     .apply_param         = bruggeman_apply_param,
+    .map_param           = bruggeman_map_param,
     .get_param_value     = bruggeman_get_param_value,
 
     .encode_param        = bruggeman_encode_param,
+    .write               = bruggeman_write,
+    .read                = bruggeman_read,
 };
 
 void
-bruggeman_free(struct disp_struct *d)
+bruggeman_free(struct disp_struct *_d)
 {
-    struct disp_bruggeman *bd = & d->disp.bruggeman;
-    assert(d->type == DISP_BRUGGEMAN);
-    disp_free(bd->comp[0]);
-    disp_free(bd->comp[1]);
-    disp_base_free(d);
+    struct disp_bruggeman *d = &_d->disp.bruggeman;
+    assert(_d->type == DISP_BRUGGEMAN);
+    if (d->disp_base) {
+        disp_free(d->disp_base);
+    }
+    for (int i = 0; i < d->components_number; i++) {
+        disp_free(d->components[i].disp);
+    }
+    free(d->components);
+    disp_base_free(_d);
 }
 
 disp_t *
 bruggeman_copy(const disp_t *src)
 {
-    struct disp_struct *res = disp_base_copy(src);
-    struct disp_bruggeman *bd = & res->disp.bruggeman;
-    bd->comp[0] = disp_copy(bd->comp[0]);
-    bd->comp[1] = disp_copy(bd->comp[1]);
-    return res;
+    struct disp_struct *new_disp = disp_base_copy(src);
+    const struct disp_bruggeman *src_bema = &src->disp.bruggeman;
+    struct disp_bruggeman *new_bema = &new_disp->disp.bruggeman;
+    new_bema->disp_base = disp_copy(src_bema->disp_base);
+    new_bema->components_number = src_bema->components_number;
+    new_bema->components = emalloc(src_bema->components_number * sizeof(struct bema_component));
+    for (int i = 0; i < new_bema->components_number; i++) {
+        new_bema->components[i].fraction = src_bema->components[i].fraction;
+        new_bema->components[i].disp = disp_copy(src_bema->components[i].disp);
+    }
+    return new_disp;
+}
+
+/* Take the ownership of the dispersions. */
+disp_t *
+bruggeman_new(const char *name, disp_t *disp_base, int components_number, struct bema_component *components)
+{
+    disp_t *new_disp = disp_new_with_name(DISP_BRUGGEMAN, name);
+    struct disp_bruggeman *bema = &new_disp->disp.bruggeman;
+    bema->disp_base = disp_base;
+    bema->components_number = components_number;
+    bema->components = emalloc(components_number * sizeof(struct bema_component));
+    memcpy(bema->components, components, components_number * sizeof(struct bema_component));
+    return new_disp;
 }
 
 cmpl
@@ -61,41 +91,134 @@ bruggeman_n_value(const disp_t *disp, double lam)
     return bruggeman_n_value_deriv(disp, lam, NULL);
 }
 
+static cmpl
+bruggeman_debug_verif(const struct disp_bruggeman *d, const cmpl eps, double lam) {
+    double f_base = 1.0;
+    for (int i = 0; i < d->components_number; i++) {
+        f_base -= d->components[i].fraction;
+    }
+    const cmpl n_base = n_value(d->disp_base, lam);
+    const cmpl eps_base = n_base * n_base;
+    const cmpl den_base = eps_base + 2 * eps;
+    cmpl sum = f_base * (eps_base - eps) / den_base;
+
+    for (int i = 0; i < d->components_number; i++) {
+        const cmpl n_i = n_value(d->components[i].disp, lam);
+        const double f_i = d->components[i].fraction;
+        const cmpl eps_i = n_i * n_i;
+        const cmpl den_i = eps_i + 2 * eps;
+        sum += f_i * (eps_i - eps) / den_i;
+    }
+    return sum;
+}
+
+static void
+bruggeman_debug_verif_deriv(const disp_t *disp, double lam, int *VERIF_DEBUG) {
+    *VERIF_DEBUG = 0;
+    const struct disp_bruggeman *bema = &disp->disp.bruggeman;
+    cmpl_vector *v_test = cmpl_vector_alloc(bema->components_number);
+    cmpl_vector *v_num  = cmpl_vector_alloc(bema->components_number);
+    const cmpl n0 = bruggeman_n_value_deriv(disp, lam, v_test);
+    for (int i = 0; i < bema->components_number; i++) {
+        const double f0 = bema->components[i].fraction;
+        const double df = 0.005;
+        bema->components[i].fraction = f0 + df;
+        cmpl n1 = bruggeman_n_value_deriv(disp, lam, NULL);
+        bema->components[i].fraction = f0;
+        cmpl_vector_set(v_num, i, (n1 - n0) / df);
+    }
+    for (int i = 0; i < bema->components_number; i++) {
+        const cmpl d_test = cmpl_vector_get(v_test, i);
+        const cmpl d_num  = cmpl_vector_get(v_num,  i);
+        printf("%3i: (%10g, %10g) (%10g, %10g)\n", i, creal(d_test), cimag(d_test), creal(d_num), cimag(d_num));
+    }
+    cmpl_vector_free(v_test);
+    cmpl_vector_free(v_num);
+    *VERIF_DEBUG = 1;
+}
+
 cmpl
 bruggeman_n_value_deriv(const disp_t *disp, double lam, cmpl_vector *v)
 {
-    const struct disp_bruggeman *d = & disp->disp.bruggeman;
-    cmpl eps, b, e1, e2, delta, n;
-    double f1, f2;
+    // TODO: cache the n values of the components.
+    const struct disp_bruggeman *d = &disp->disp.bruggeman;
+    double f_base = 1.0;
+    for (int i = 0; i < d->components_number; i++) {
+        f_base -= d->components[i].fraction;
+    }
+    const cmpl n_base = n_value(d->disp_base, lam);
+    const cmpl eps_base = n_base * n_base;
+    cmpl eps0 = f_base * eps_base;
 
-    e1 = n_value(d->comp[0], lam);
-    e1 *= e1;
-
-    e2 = n_value(d->comp[1], lam);
-    e2 *= e2;
-
-    f1 = d->frac[0];
-    f2 = d->frac[1];
-
-    b = 2 * f1 * e1 + 2 * f2 * e2 - f1 * e2 - f2 * e1;
-    delta = b*b + 8 * e1 * e2;
-
-    eps = 1/4.0 * (b + csqrt(delta));
-    n = csqrt(eps);
-
-    if(v != NULL) {
-        cmpl der = 1/(2*n) * (3 * eps * (e1 - e2)) / csqrt(delta);
-        cmpl_vector_set(v, 0, der);
-        cmpl_vector_set(v, 1, -der);
+    for (int i = 0; i < d->components_number; i++) {
+        const cmpl n = n_value(d->components[i].disp, lam);
+        eps0 += d->components[i].fraction * n * n;
     }
 
-    return n;
+    cmpl eps1;
+    cmpl sum_fe, sum_f1, sum_fesq;
+    int final_iteration = 0;
+    while (1) {
+        const cmpl den = eps_base + 2 * eps0;
+        sum_fe = f_base * eps_base / den;
+        sum_f1 = f_base / den;
+        const cmpl den_square = den * den;
+        sum_fesq = 3 * f_base * eps_base / den_square;
+
+        for (int i = 0; i < d->components_number; i++) {
+            const cmpl n_i = n_value(d->components[i].disp, lam);
+            const double f = d->components[i].fraction;
+            const cmpl eps_i = n_i * n_i;
+            const cmpl den_i = eps_i + 2 * eps0;
+            const cmpl den_square_i = den_i * den_i;
+            sum_fe += f * eps_i / den_i;
+            sum_f1 += f / den_i;
+            sum_fesq += 3 * f * eps_i / den_square_i;
+        }
+
+        if (final_iteration) {
+            break;
+        }
+
+        eps1 = sum_fe / sum_f1;
+        cmpl delta_eps = eps1 - eps0;
+        if (fabs(creal(delta_eps)) < 1e-4 && fabs(cimag(delta_eps)) < 1e-4) {
+            final_iteration = 1;
+        }
+
+        eps0 = eps1;
+    }
+
+    if(v != NULL) {
+        for (int i = 0; i < d->components_number; i++) {
+            const cmpl n_i = n_value(d->components[i].disp, lam);
+            const cmpl eps_i = n_i * n_i;
+            const cmpl resid_i = (eps_i - eps1) / (eps_i + 2 * eps1);
+            const cmpl eps_der_i = resid_i / sum_fesq + eps1;
+            cmpl n_der_i = 1.0 / (2.0 * csqrt(eps1)) * eps_der_i;
+
+            const cmpl resid_base = (eps_base - eps1) / (eps_base + 2 * eps1);
+            const cmpl eps_der_base = resid_base / sum_fesq + eps1;
+            cmpl n_der_base = 1.0 / (2.0 * csqrt(eps1)) * eps_der_base;
+
+            cmpl_vector_set(v, i, n_der_i - n_der_base);
+        }
+    }
+
+    static int VERIF_DEBUG = 1;
+    if (VERIF_DEBUG) {
+        cmpl sum_verif = bruggeman_debug_verif(d, eps1, lam);
+        bruggeman_debug_verif_deriv(disp, lam, &VERIF_DEBUG);
+    }
+
+    return csqrt(eps1);
 }
 
 int
 bruggeman_fp_number(const disp_t *disp)
 {
-    return 2;
+    const struct disp_bruggeman *d = & disp->disp.bruggeman;
+    return d->components_number;
 }
 
 void
@@ -109,23 +232,8 @@ bruggeman_apply_param(struct disp_struct *d, const fit_param_t *fp,
                       double val)
 {
     struct disp_bruggeman *bd = & d->disp.bruggeman;
-
     assert(d->type == DISP_BRUGGEMAN);
-
-    if(fp->model_id != DISP_BRUGGEMAN) {
-        return 1;
-    }
-
-    if(fp->param_nb == 0) {
-        bd->frac[0] = val;
-        bd->frac[1] = 1.0 - val;
-    } else if(fp->param_nb == 1) {
-        bd->frac[1] = val;
-        bd->frac[0] = 1.0 - val;
-    } else {
-        return 1;
-    }
-
+    bd->components[fp->param_nb].fraction = val;
     return 0;
 }
 
@@ -134,7 +242,52 @@ bruggeman_get_param_value(const struct disp_struct *d,
                           const fit_param_t *fp)
 {
     const struct disp_bruggeman *bd = & d->disp.bruggeman;
-    int np = fp->param_nb;
-    assert(np >= 0 && np < 2);
-    return bd->frac[np];
+    return bd->components[fp->param_nb].fraction;
+}
+
+double *
+bruggeman_map_param(disp_t *d, int index) {
+    struct disp_bruggeman *bd = & d->disp.bruggeman;
+    return &bd->components[index].fraction;
+}
+
+int bruggeman_write(writer_t *w, const disp_t *d) {
+    const struct disp_bruggeman *bema = &d->disp.bruggeman;
+    writer_printf(w, "%d", bema->components_number);
+    writer_newline_enter(w);
+    disp_write(w, bema->disp_base);
+    for (int i = 0; i < bema->components_number; i++) {
+        writer_printf(w, "%g", bema->components[i].fraction);
+        writer_newline(w);
+        disp_write(w, bema->components[i].disp);
+    }
+    writer_indent(w, -1);
+    return 0;
+}
+
+int bruggeman_read(lexer_t *l, disp_t *d) {
+    struct disp_bruggeman *bema = &d->disp.bruggeman;
+    bema->disp_base = NULL;
+    bema->components_number = 0;
+    bema->components = NULL;
+    int components_number;
+    if (lexer_integer(l, &components_number)) return 1;
+    bema->disp_base = disp_read(l);
+    if (!bema->disp_base) return 1;
+    struct bema_component *components = emalloc(components_number * sizeof(struct bema_component));
+    int i;
+    for (i = 0; i < components_number; i++) {
+        if (lexer_number(l, &components[i].fraction)) goto read_exit_clean;
+        components[i].disp = disp_read(l);
+        if (!components[i].disp) goto read_exit_clean;
+    }
+    bema->components_number = components_number;
+    bema->components = components;
+    return 0;
+read_exit_clean:
+    for (int k = 0; k < i; k++) {
+        disp_free(components[k].disp);
+    }
+    free(components);
+    return 1;
 }
--- src/disp-bruggeman.h
+++ src/disp-bruggeman.h
@@ -1,14 +1,27 @@
 #ifndef DISP_BRUGGEMAN_H
 #define DISP_BRUGGEMAN_H
 
+#include "defs.h"
+
+__BEGIN_DECLS
+
 struct disp_struct;
 
+struct bema_component {
+    double fraction;
+    struct disp_struct *disp;
+};
+
 struct disp_bruggeman {
-    /* two components Bruggeman EMA model */
-    double frac[2];
-    struct disp_struct *comp[2];
+    struct disp_struct *disp_base;
+    int components_number;
+    struct bema_component *components;
 };
 
 extern struct disp_class bruggeman_disp_class;
+
+extern struct disp_struct *bruggeman_new(const char *name, struct disp_struct *disp_base, int components_number, struct bema_component *components);
+
+__END_DECLS
 
 #endif
