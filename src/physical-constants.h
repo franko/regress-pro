b9a41d9361ba3171d8db3f77db98315791f65754
Francesco <francesco.bbt@gmail.com>
2017-08-14 23:26:08 +0200

    More accurate value of wavelength to energy conversion constant

--- src/physical-constants.h
+++ src/physical-constants.h
@@ -4,9 +4,12 @@
 /*
    For the conversion from eV to nm the value EV_TO_NM defined below is used:
    E(eV) = EV_TO_NM / wavelength(nm)
+
+    The more accurate value found using constants on NIST would be: 1239.841973862093
+    (source: https://physics.nist.gov/cgi-bin/cuu/Value?h|search_for=universal_in! )
 */
 
-#define EV_TO_NM 1240.0
+#define EV_TO_NM 1239.852
 
 #endif
 
