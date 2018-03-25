7240146b19df4ecb10d96b6a5c447842106c306f
Francesco Abbate <francesco.bbt@gmail.com>
2018-03-25 15:20:14 +0200

    Try to use sympy generated code for Tauc-Lorentz model
    Derivatives seems to be wrong.

--- src/disp-tauc-lorentz.cpp
+++ src/disp-tauc-lorentz.cpp
@@ -59,12 +59,13 @@ static const char *tauc_lorentz_param_names[] = {"AL", "E0", "C"};
 #define TL_NB_GLOBAL_PARAMS 2
 #define TL_NINF_OFFS 0
 #define TL_EG_OFFS 1
-#define TL_NB_PARAMS 3
+#define TL_OSC_OFFS 2
+#define TL_NB_OSC_PARAMS 3
 #define TL_AL_OFFS  0
 #define TL_E0_OFFS  1
 #define TL_C_OFFS  2
 
-#define TL_PARAM_NB(hn,pn) (TL_NB_PARAMS * (hn) + pn)
+#define TL_PARAM_NB(hn,pn) (TL_NB_OSC_PARAMS * (hn) + pn)
 
 static double egap_k(const double E, const double Eg, const double k)
 {
@@ -172,7 +173,6 @@ tauc_lorentz_n_value(const disp_t *d, double lambda)
 {
     const struct disp_fb *fb = &d->disp.fb;
     const int nb = fb->n;
-    // double er_sum = fb->n_inf, ei_sum = 0;
     double en = TL_EV_NM / lambda;
 
     /* If Eg is negative use zero instead. A negative Eg is not meaningful. */
@@ -232,6 +232,167 @@ tauc_lorentz_n_value(const disp_t *d, double lambda)
     return std::sqrt(er_sum - 1i * ei_sum);
 }
 
+cmpl
+tauc_lorentz_n_value_deriv(const disp_t *d, double lambda, cmpl_vector *pd)
+{
+    const struct disp_fb *fb = &d->disp.fb;
+    const int nb = fb->n;
+    double en = TL_EV_NM / lambda;
+
+    /* If Eg is negative use zero instead. A negative Eg is not meaningful. */
+    const double eg = (fb->eg >= 0 ? fb->eg : 0.0);
+
+    pd->at(TL_NINF_OFFS) = cmpl(1.0, 0.0);
+    pd->at(TL_EG_OFFS) = 0;
+
+    double er_sum = fb->n_inf, ei_sum = 0.0;
+    for (int osc_index = TL_OSC_OFFS, k = 0; k < nb; k++, osc_index += TL_NB_OSC_PARAMS) {
+        const struct fb_osc *osc = fb->osc + k;
+        double a, e0, c;
+        if (fb->form == TAUC_LORENTZ_STANDARD) {
+            a = osc->a;
+            e0 = osc->b;
+            c = osc->c;
+        } else {
+            /* Use rationalized lorentzian coefficients of the abs peak:
+               - osc->a is AL' and gives the height of the peak
+               - osc->b is Ep and is the energy position
+               - osc->c is Gamma and is the peak width. */
+            e0 = pow(pow4(osc->b) + pow4(osc->c) / 4, 0.25);
+            c = sqrt(2 * pow2(e0) - 2 * pow2(osc->b));
+            a = osc->a * pow4(osc->c) / (4 * e0 * c);
+        }
+
+        double x0 = std::pow(eg, 2);
+        double x1 = std::pow(en, 2);
+        double x2 = x0 + x1;
+        double x3 = 1.0/M_PI;
+        double x4 = 1.0/en;
+        double x5 = eg + en;
+        double x6 = log(x5);
+        double x7 = -eg + en;
+        bool   x8 = (en > eg);
+        double x9 = eg - en;
+        double x10 = x8 ? log(x7) : log(x9);
+        double x11 = x10 - x6;
+        double x12 = std::pow(c, 2);
+        double x13 = -x12;
+        double x14 = std::pow(e0, 2);
+        double x15 = x13 + 4*x14;
+        double x16 = -x14;
+        double x17 = x1 + x16;
+        double x18 = (1.0L/2.0L)*x12 + x17;
+        double x19 = (1.0L/4.0L)*x12*x15 + std::pow(x18, 2);
+        double x20 = 1.0/x19;
+        double x21 = a*c*x11*x20*x3*x4;
+        double x22 = x2*x21;
+        double x23 = 2*e0*eg;
+        double x24 = x0*x12;
+        double x25 = -x0 + x14;
+        double x26 = x24 + std::pow(x25, 2);
+        double x27 = x10 + x6 - log(std::sqrt(x26));
+        double x28 = a*c*x20*x27*x3;
+        double x29 = std::sqrt(x15);
+        double x30 = 1.0/x29;
+        double x31 = 1.0/c;
+        double x32 = -2*x0 + x13 + 2*x14;
+        double x33 = x30*x32;
+        double x34 = 2*atan(x31*x33) + M_PI;
+        double x35 = a*x18*x20*x3*x30*x34;
+        double x36 = x0 + x14;
+        double x37 = x17*x36 + x24;
+        double x38 = 1.0/e0;
+        double x39 = 2*eg;
+        double x40 = x29 - x39;
+        double x41 = x29 + x39;
+        double x42 = atan(x31*x40) - atan(x31*x41) + M_PI;
+        double x43 = a*x20*x3*x38*x42;
+        double x44 = x0 + x16;
+        double x45 = 3*x0 + x14;
+        double x46 = x1*x44 - x14*x45 + x24;
+        double x47 = eg*x29;
+        double x48 = x36 + x47;
+        double x49 = x36 - x47;
+        double x50 = 1.0/x49;
+        double x51 = log(x48*x50);
+        double x52 = (1.0L/2.0L)*a*c*x20*x3*x30*x38*x51;
+        double x53 = std::pow(x7, 2);
+        double x54 = x1*x12 + std::pow(x17, 2);
+        double x55 = 1.0/x54;
+        double x56 = a*x4*x53*x55;
+        double x57 = c*x56;
+        double x58 = a*e0*x20*x3;
+        double x59 = 1.0/x5;
+        double x60 = x8 ? -1/x7 : 1.0/x9;
+        double x61 = 2*e0;
+        double x62 = 2*a*e0*x20*x3*x34;
+        double x63 = x12*x39;
+        double x64 = 1.0/x26;
+        double x65 = 1.0/x15;
+        double x66 = 1.0/x12;
+        double x67 = 1.0/(std::pow(x32, 2)*x65*x66 + 1);
+        double x68 = 2*x31;
+        double x69 = 1.0/(std::pow(x40, 2)*x66 + 1);
+        double x70 = 1.0/(std::pow(x41, 2)*x66 + 1);
+        double x71 = a*x20*x3*x37*x38;
+        double x72 = x48/std::pow(x49, 2);
+        double x73 = (1.0L/2.0L)*a*c*x20*x3*x30*x38*x46*x49/x48;
+        double x74 = x20*x3;
+        double x75 = 2*e0*eg*x18*x30*x34;
+        double x76 = x37*x38*x42;
+        double x77 = (1.0L/2.0L)*c*x30*x38*x46*x51;
+        double x78 = 4*eg*x14;
+        double x79 = 4*e0*x18 - x12*x61;
+        double x80 = std::pow(x19, -2);
+        double x81 = a*c*e0*x11*x2*x3*x4*x80;
+        double x82 = std::pow(x15, -3.0L/2.0L);
+        double x83 = a*x20*x3*x34*x82;
+        double x84 = a*x20*x3/x14;
+        double x85 = 2*a*c*e0*eg*x27*x3*x80;
+        double x86 = a*x3*x79*x80;
+        double x87 = x46*x51*x82;
+        double x88 = 4*e0*x30;
+        double x89 = x31*x88;
+        double x90 = x32*x82;
+        double x91 = 4*a*e0*eg*x18*x20*x3*x30*x67;
+        double x92 = eg*x88;
+        double x93 = c*eg*x30;
+        double x94 = a*x20*x3*x30*x38*x51;
+        double x95 = (1.0L/2.0L)*std::pow(c, 3) - 1.0L/2.0L*c*x15 - 2*c*x18;
+        double x96 = a*x3*x80*x95;
+        double x97 = -x30;
+        double x98 = c*e0*x4*x55;
+        double x99 = std::pow(x54, -2);
+
+        er_sum += -e0*x22 + x23*x28 + x23*x35 - x37*x43 + x46*x52;
+        ei_sum += (x8 ? e0*x57 : 0);
+
+        const double d_eg_real = 2*a*c*e0*eg*x20*x3*(x59 + x60 - x64*(eg*x12 - x25*x39)) - c*x2*x4*x58*(-x59 + x60) - 16*x0*x18*x31*x58*x65*x67 + x18*x30*x62 - x21*x23 + x28*x61 - x43*(x17*x39 + x63) + x52*(-6*eg*x14 + x1*x39 + x63) - x71*(-x68*x69 - x68*x70) + x73*(x40*x72 + x41*x50);
+        const double d_eg_imag = (x8 ? a*x98*(-2*en + x39) : 0);
+        pd->at(TL_EG_OFFS) += cmpl(d_eg_real, d_eg_imag);
+
+        const double d_a_real = 2*c*e0*eg*x27*x74 - c*e0*x11*x2*x4*x74 + x74*x75 - x74*x76 + x74*x77;
+        const double d_a_imag = (x8 ? x53*x98 : 0);
+        pd->at(osc_index + TL_AL_OFFS) = cmpl(d_a_real, d_a_imag);
+
+        const double d_e0_real = -a*c*x20*x25*x3*x64*x78 - 2*a*c*x20*x3*x87 - a*x20*x3*x30*x34*x78 - 1.0L/2.0L*c*x30*x46*x51*x84 - 8*eg*x14*x18*x83 - x22 + x28*x39 + x35*x39 + x37*x42*x84 - x43*(x17*x61 - x36*x61) + x52*(-2*std::pow(e0, 3) - x1*x61 - x45*x61) - x71*(x69*x89 - x70*x89) + x73*(x50*(x61 + x92) + x72*(-x61 + x92)) + x75*x86 - x76*x86 + x77*x86 - x79*x81 + x79*x85 + x91*(-4*e0*x31*x90 + x89);
+        const double d_e0_imag = (x8 ? 4*a*c*x14*x17*x4*x53*x99 + x57 : 0);
+        pd->at(osc_index + TL_E0_OFFS) = cmpl(d_e0_real, d_e0_imag);
+
+        const double d_c_real = -2*a*e0*std::pow(eg, 3)*x12*x20*x3*x64 + 2*a*e0*eg*x20*x27*x3 - a*e0*x11*x2*x20*x3*x4 + (1.0L/2.0L)*a*x12*x20*x3*x38*x87 + 2*c*e0*eg*x18*x83 - 2*c*x0*x43 + x24*x94 + x62*x93 - x71*(x69*(-x40*x66 + x97) - x70*(-x41*x66 + x97)) + x73*(-x50*x93 - x72*x93) + x75*x96 - x76*x96 + x77*x96 - x81*x95 + x85*x95 + x91*(-2*x30 - x33*x66 + x90) + x94*((1.0L/2.0L)*x0*x12 + (1.0L/2.0L)*x1*x44 - 1.0L/2.0L*x14*x45);
+        const double d_c_imag = (x8 ? -2*a*e0*en*x12*x53*x99 + e0*x56 : 0);
+        pd->at(osc_index + TL_C_OFFS)  = cmpl(d_c_real, d_c_imag);
+    }
+
+    cmpl nn = sqrt(cmpl(er_sum, -ei_sum));
+
+    for (int i = 0; i < TL_NB_GLOBAL_PARAMS + nb * TL_NB_OSC_PARAMS; i++) {
+        pd->at(i) = cmpl(1, 0) / (2 * nn) * pd->at(i);
+    }
+
+    return nn;
+}
+
 void tauc_lorentz_change_form(struct disp_fb *fb, int new_coeff_form)
 {
     if (new_coeff_form == TAUC_LORENTZ_STANDARD) {
@@ -263,7 +424,7 @@ void tauc_lorentz_change_form(struct disp_fb *fb, int new_coeff_form)
         }
     }
 }
-
+#if 0
 struct eval_param {
     disp_t *d;
     double lambda;
@@ -337,6 +498,7 @@ tauc_lorentz_n_value_deriv(const disp_t *d, double lambda, cmpl_vector *pd)
     }
     return n;
 }
+#endif
 
 void
 tauc_lorentz_encode_param(str_t param, const fit_param_t *fp)
@@ -346,8 +508,8 @@ tauc_lorentz_encode_param(str_t param, const fit_param_t *fp)
     } else if (fp->param_nb == 1) {
         str_copy_c(param, "Eg");
     } else {
-        int onb = (fp->param_nb - TL_NB_GLOBAL_PARAMS) / TL_NB_PARAMS;
-        int pnb = (fp->param_nb - TL_NB_GLOBAL_PARAMS) % TL_NB_PARAMS;
+        int onb = (fp->param_nb - TL_NB_GLOBAL_PARAMS) / TL_NB_OSC_PARAMS;
+        int pnb = (fp->param_nb - TL_NB_GLOBAL_PARAMS) % TL_NB_OSC_PARAMS;
         str_printf(param, "%s:%i", tauc_lorentz_param_names[pnb], onb);
     }
 }
