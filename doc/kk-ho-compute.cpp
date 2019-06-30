9a9ed3c1001d9c7ce5f6a3f75de28eed31e71a03
Francesco Abbate <francesco.bbt@gmail.com>
2019-07-01 00:06:35 +0200

    Add plotting with libcanvas in KK HO modle test

--- doc/kk-ho-compute.cpp
+++ doc/kk-ho-compute.cpp
@@ -1,7 +1,11 @@
+#include "window.h"
+#include "debug_log.h"
+#include "path.h"
+
 #include <cstdio>
 #include <gsl/gsl_integration.h>
 
-constexpr double pi() { return 3.14159265358979323846; }
+constexpr double m_pi() { return 3.14159265358979323846; }
 
 template <typename Float> Float sqr(Float x) { return x * x; }
 
@@ -21,22 +25,26 @@ double hoRealPart(const HOModel& ho, double e) {
 static double hoKKImgPartFfun(double x, void *param) {
     const HOModel *ho = (const HOModel *) param;
     double chi2 = hoImaginaryPart(*ho, x);
-    return (2 / pi()) * x * chi2 / (sqr(x) - sqr(ho->e));
+    return (2 / m_pi()) * x * chi2 / (sqr(x) - sqr(ho->e));
 }
 
 static double hoImgPartFfun(double x, void *param) {
     const HOModel *ho = (const HOModel *) param;
     double chi2 = hoImaginaryPart(*ho, x);
-    return (2 / pi()) * x * chi2 / (x + ho->e);
+    return (2 / m_pi()) * x * chi2 / (x + ho->e);
 }
 
 int main() {
+    graphics::initialize_fonts();
+    graphics::window win;
+    graphics::plot p(graphics::plot::show_units | graphics::plot::auto_limits);
+    p.set_clip_mode(false);
+
     const int intLimit = 128;
     gsl_integration_workspace *gs = gsl_integration_workspace_alloc(intLimit);
     const double eCut = 12.0;
 
     HOModel hoTest{3.5, 0.9, 10.0, 0.0};
-    hoTest.e = 0.0;
 
     gsl_function hoImgPartF;
     hoImgPartF.function = &hoImgPartFfun;
@@ -47,6 +55,9 @@ int main() {
     hoKKImgPartF.params = &hoTest;
 
     double epsabs = 1e-5, epsrel = 1e-5;
+
+    auto lineN = new graphics::path();
+    auto lineK = new graphics::path();
 
     const double e1 = 0.5, e2 = 8.0;
     for (int i = 0; i <= 128; i++) {
@@ -61,7 +72,30 @@ int main() {
         gsl_integration_qagiu(&hoKKImgPartF, eCut, epsabs, epsrel, intLimit, gs, &result2, &abserr2);
 
         printf("%g, %g\n", result1 + result2, hoRealPart(hoTest, hoTest.e));
+
+        if (i == 0) {
+            lineN->move_to(eEval, result1 + result2);
+        } else {
+            lineN->line_to(eEval, result1 + result2);
+        }
+
+        if (i == 0) {
+            lineK->move_to(eEval, -hoImaginaryPart(hoTest, hoTest.e));
+        } else {
+            lineK->line_to(eEval, -hoImaginaryPart(hoTest, hoTest.e));
+        }
     }
+
+    agg::rgba8 none(0,0,0,0);
+    agg::rgba8 blue(0, 0, 180, 255);
+    agg::rgba8 red(180, 0, 0, 255);
+    p.add(lineN, blue, 1.5, none, graphics::property::stroke);
+    p.add(lineK, red,  1.5, none, graphics::property::stroke);
+    int index = win.attach(&p, "");
+    win.start(640, 480, graphics::window_resize);
+    win.slot_refresh(index);
+    p.commit_pending_draw();
+    win.wait();
 
     return 0;
 }
