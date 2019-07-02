1040194ca12a4af1a436d4be76825557523779d3
Francesco Abbate <francesco.bbt@gmail.com>
2019-07-02 21:39:23 +0200

    Add phi parameters in HO model for KK testing

--- doc/kk-ho-compute.cpp
+++ doc/kk-ho-compute.cpp
@@ -10,16 +10,20 @@ constexpr double m_pi() { return 3.14159265358979323846; }
 template <typename Float> Float sqr(Float x) { return x * x; }
 
 struct HOModel {
-    double en, eg, a; // phi;
+    double en, eg, a, phi;
     double e;
 };
 
 double hoImaginaryPart(const HOModel& ho, double e) {
-    return - (ho.a * ho.eg * e) / (sqr(sqr(ho.en) - sqr(e)) + sqr(ho.eg) * sqr(e));
+    double dr = sqr(ho.en) - sqr(e), di = ho.eg * e;
+    double c = std::cos(ho.phi), s = std::sin(ho.phi);
+    return ho.a * (-c * di + s * dr) / (sqr(dr) + sqr(di));
 }
 
 double hoRealPart(const HOModel& ho, double e) {
-    return (ho.a * (sqr(ho.en) - sqr(e))) / (sqr(sqr(ho.en) - sqr(e)) + sqr(ho.eg) * sqr(e));
+    double dr = sqr(ho.en) - sqr(e), di = ho.eg * e;
+    double c = std::cos(ho.phi), s = std::sin(ho.phi);
+    return ho.a * (c * dr + s * di) / (sqr(dr) + sqr(di));
 }
 
 static double hoKKImgPartFfun(double x, void *param) {
@@ -40,11 +44,11 @@ int main() {
     graphics::plot p(graphics::plot::show_units | graphics::plot::auto_limits);
     p.set_clip_mode(false);
 
-    const int intLimit = 128;
-    gsl_integration_workspace *gs = gsl_integration_workspace_alloc(intLimit);
+    const int integLimit = 256;
+    gsl_integration_workspace *gs = gsl_integration_workspace_alloc(integLimit);
     const double eCut = 12.0;
 
-    HOModel hoTest{3.5, 0.9, 10.0, 0.0};
+    HOModel hoTest{3.5, 0.3, 10.0, m_pi() / 4, 0.0};
 
     gsl_function hoImgPartF;
     hoImgPartF.function = &hoImgPartFfun;
@@ -56,27 +60,34 @@ int main() {
 
     double epsabs = 1e-5, epsrel = 1e-5;
 
-    auto lineN = new graphics::path();
+    auto lineN = new graphics::path(), lineNRef = new graphics::path();
     auto lineK = new graphics::path();
 
-    const double e1 = 0.5, e2 = 8.0;
-    for (int i = 0; i <= 128; i++) {
-        const double eEval = e1 + (e2 - e1) * i / 128;
+    const double e1 = 0.01, e2 = 10.0;
+    const int nIntervals = 256;
+    for (int i = 0; i <= 256; i++) {
+        const double eEval = e1 + (e2 - e1) * i / nIntervals;
 
         hoTest.e = eEval;
 
         double result1, abserr1;
-        gsl_integration_qawc(&hoImgPartF, 0.0, eCut, eEval, epsabs, epsrel, intLimit, gs, &result1, &abserr1);
+        gsl_integration_qawc(&hoImgPartF, 0.0, eCut, eEval, epsabs, epsrel, integLimit, gs, &result1, &abserr1);
 
         double result2, abserr2;
-        gsl_integration_qagiu(&hoKKImgPartF, eCut, epsabs, epsrel, intLimit, gs, &result2, &abserr2);
+        gsl_integration_qagiu(&hoKKImgPartF, eCut, epsabs, epsrel, integLimit, gs, &result2, &abserr2);
 
-        printf("%g, %g\n", result1 + result2, hoRealPart(hoTest, hoTest.e));
+        printf("%g, %g, %g\n", eEval, result1 + result2, hoRealPart(hoTest, hoTest.e));
 
         if (i == 0) {
             lineN->move_to(eEval, result1 + result2);
         } else {
             lineN->line_to(eEval, result1 + result2);
+        }
+
+        if (i == 0) {
+            lineNRef->move_to(eEval, -hoRealPart(hoTest, hoTest.e));
+        } else {
+            lineNRef->line_to(eEval, -hoRealPart(hoTest, hoTest.e));
         }
 
         if (i == 0) {
@@ -89,6 +100,8 @@ int main() {
     agg::rgba8 none(0,0,0,0);
     agg::rgba8 blue(0, 0, 180, 255);
     agg::rgba8 red(180, 0, 0, 255);
+    agg::rgba8 gray(50, 50, 50, 255);
+    p.add(lineNRef, gray, 1.0, none, graphics::property::stroke);
     p.add(lineN, blue, 1.5, none, graphics::property::stroke);
     p.add(lineK, red,  1.5, none, graphics::property::stroke);
     int index = win.attach(&p, "");
