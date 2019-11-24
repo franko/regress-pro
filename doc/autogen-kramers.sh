eb9795f7e21c666e5d977741c62155cc20505a5c
Francesco Abbate <francesco.bbt@gmail.com>
2019-11-24 11:43:39 +0100

    Improve sympy building script for Kramers model

--- doc/autogen-kramers.sh
+++ doc/autogen-kramers.sh
@@ -1,6 +1,23 @@
+cd "$(dirname "$0")"
+
+if ! python -c "import sympy; print('')" &> /dev/null; then
+    echo "The sympy module is not available."
+    echo "You may install sympy and try again."
+    exit 1
+fi
+
+format_code () {
+    clang-format -i -style="{BasedOnStyle: google, IndentWidth: 4}" "$1"
+}
+
+echo "Kramers oscillators model"
+
+echo "Generating C++ kernel for n evaluation..."
 python kramers-sympy.py ../src/kramers-template.cpp > ../src/kramers-kernel.cpp || exit 1
-clang-format -i -style="{BasedOnStyle: google, IndentWidth: 4}" ../src/kramers-kernel.cpp
+format_code ../src/kramers-kernel.cpp
 
+echo "Generating C++ kernel for n evaluation and its derivatives..."
 python kramers-diff-sympy.py ../src/kramers-diff-template.cpp > ../src/kramers-diff-kernel.cpp || exit 1
-clang-format -i -style="{BasedOnStyle: google, IndentWidth: 4}" ../src/kramers-diff-kernel.cpp
+format_code ../src/kramers-diff-kernel.cpp
 
+echo "Done"
